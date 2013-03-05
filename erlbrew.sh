85f7c29d0365a2ff4425f5c1b4733793a76004c7
Mark Allen <mrallen1@yahoo.com>
2013-03-04 23:02:11 -0600

    Use functions and save work when possible

--- erlbrew.sh
+++ erlbrew.sh
@@ -7,30 +7,69 @@ RELEASE=$2
 WORK="$HOME/erlang"
 BUILD="$WORK/.build"
 INSTALL="$WORK/$RELEASE"
-DOWNLOAD_FILE="otp_src_$RELEASE.tar.gz"
+FILENAME="otp_src_$RELEASE"
+DOWNLOAD_FILE="$FILENAME.tar.gz"
+TARBALL_PATH="$BUILD/tarballs"
+TMP_PATH="$BUILD/work.$$"
+DOWNLOAD_PATH="$BUILD/tarballs/$DOWNLOAD_FILE"
 DOWNLOAD_URL="http://www.erlang.org/download/$DOWNLOAD_FILE"
-# http://www.erlang.org/download/otp_src_R14B04.tar.gz
-DOWNLOAD_TOOL="curl -o"
-DOWNLOAD_CMD="$DOWNLOAD_TOOL $BUILD/$DOWNLOAD_FILE $DOWNLOAD_URL"
-UNTAR_CMD="tar -zxvf"
+DOWNLOAD_TOOL=$(which curl)
+DOWNLOAD_TOOL_FLAGS="--fail --progress-bar --show-error"
+DOWNLOAD_CMD="$DOWNLOAD_TOOL $DOWNLOAD_TOOL_FLAGS --output $DOWNLOAD_PATH $DOWNLOAD_URL"
+UNTAR_CMD=$(which tar)
+UNTAR_FLAGS="-zxvf"
 
 download() {
-  mkdir -p $BUILD
-  $DOWNLOAD_CMD
+  if [ ! -d "$TARBALL_PATH" ]; then
+    mkdir -p "$TARBALL_PATH"
+  fi
+
+  if [ ! -e "$DOWNLOAD_PATH" ]; then
+    $DOWNLOAD_CMD
+  else
+    echo "You appear to have Erlang $RELEASE downloaded."
+  fi
 }
 
 build() {
-  cd $BUILD
-  $UNTAR_CMD $DOWNLOAD_FILE
-  cd "otp_src_$RELEASE"
+  mkdir -p "$TMP_PATH"
+  cp -a "$DOWNLOAD_PATH" "$TMP_PATH"
+  cd "$TMP_PATH"
+  $UNTAR_CMD $UNTAR_FLAGS $DOWNLOAD_FILE
+  cd "$FILENAME"
   ./configure --prefix="$INSTALL" --enable-darwin-64bit --without-javac
   make
 }
 
 install() {
-  mkdir -p $INSTALL
+  if [ ! -d "$INSTALL" ]; then
+    mkdir -p $INSTALL
+  fi
+
   make install
 }
 
+case "$CMD" in
+    download)
+        download
+        ;;
+    build)
+        download
+        build
+        ;;
+    install)
+        download
+        build
+        install
+        ;;
+    use)
+        echo "Not implemented yet"
+        exit 1
+        ;;
+    *)
+        echo "Usage: $0 {download|build|install|use} {release-spec}"
+        exit 1
+
+esac
 
 
